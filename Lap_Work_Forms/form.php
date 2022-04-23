8177aa72607a4d830ec8482c7edcea9a0745ffac
otnasusagus <otnasussuga.gh@gmail.com>
2022-04-23 14:20:44 +0700

    Lab Work Forms

---
+++ Lap_Work_Forms/form.php
@@ -0,0 +1,24 @@
+<html>
+    <head>
+        <title>Form</title>
+    </head>
+    <body>
+        <h2>Student Union Form</h2>
+        <form method="post" action="proses.php">
+            Name: <input type=" text" name="name">
+            <br><br>
+            E-mail: <input type=" text" name="email">
+            <br><br>
+            Website: <input type=" text" name="website">
+            <br><br>
+            Comment: <textarea name="comment" rows="5" cols="40"></textarea>
+            <br><br>
+            Gender
+            <input type=" radio" name="gender" value="female">Female
+            <input type="radio" name="gender" value="male" >Male
+            <input type="radio" name="gender" value="other">0ther
+            <br><br>
+            <input type="submit" name="submit" value="Submit">
+        </form>
+    </body>
+</html>
